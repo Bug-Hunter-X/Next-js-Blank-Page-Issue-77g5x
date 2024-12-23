4eec63e8a82326417a9230f173e100ddea4fae6f
BugHunter-X <askzenai@gmail.com>
2024-12-23 03:50:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,14 @@
+```javascript
+// pages/index.js
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My Next.js App</title>
+      </Head>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+```
